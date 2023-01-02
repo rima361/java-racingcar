4b7de135ca9f5bf9050bc7dd5d0ef1e94e323c5c
berry.nice <berry.nice@kakaocorp.com>
2023-01-02 20:34:12 +0900

    refactor: positiveNumber의 value를 외부로 노출하지 않는 방식으로 리팩토링

--- src/main/java/calculator/string/StringAddCalculator.java
+++ src/main/java/calculator/string/StringAddCalculator.java
@@ -1,15 +1,17 @@
 package calculator.string;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import calculator.PositiveNumber;
 
 public class StringAddCalculator {
 
     public static long add(final String value) {
-        return Arrays.stream(StringSplitter.split(value))
-                     .map(PositiveNumber::new)
-                     .map(PositiveNumber::getValue)
-                     .reduce(0L, Long::sum);
+        final List<PositiveNumber> numbers = Arrays.stream(StringSplitter.split(value))
+                                                   .map(PositiveNumber::of)
+                                                   .collect(Collectors.toList());
+        return PositiveNumber.sum(numbers);
     }
 }
